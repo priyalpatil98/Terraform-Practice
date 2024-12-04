0e80e990dd75235d92d2706193d716fdd7d77249
priyalpatil98 <priyalpatil98@gmail.com>
2024-12-04 18:35:14 +0000

    Practice 2 code

---
+++ practice2/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "my_vpc_1" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "prod"
+    }
+}
+
+resource "aws_subnet" "subnet-1" {
+    vpc_id = aws_vpc.my_vpc_1.id
+    cidr_block = "10.0.1.0/24"
+
+    tags = {
+      Name = "prod-subnet-1"
+    }
+  
+}
